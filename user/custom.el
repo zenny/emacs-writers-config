1a91252093b99233c91c8f568ef2bb36aef1f71e
owl <m2eadams@gmail.com>
2017-11-22 08:31:42 -0800

    Updates package-selceted-packages

--- user/custom.el
+++ user/custom.el
@@ -3,14 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#333333" "#dddddd" "#aaaaaa" "#ffffff" "#ffffff" "#dddddd" "#ffffff" "#ffffff"])
  '(custom-safe-themes
    (quote
-    ("1f3113447a652b8436a9938bbac71ecaf022cc73ecd0d76182eb9713aa781f17" default)))
- '(fci-rule-color "#222222")
- '(pdf-view-midnight-colors (quote ("#ffffff" . "#222222")))
- '(vc-annotate-background "#222222"))
+    ("4bfced46dcfc40c45b076a1758ca106a947b1b6a6ff79a3281f3accacfb3243c" "1f3113447a652b8436a9938bbac71ecaf022cc73ecd0d76182eb9713aa781f17" default)))
+ '(package-selected-packages
+   (quote
+    (wttrin elfeed ox-tufte pandoc markdown-mode gmail-message-mode orca writeroom-mode use-package orgtbl-ascii-plot org-plus-contrib olivetti leuven-theme gnuplot f eziam-theme exec-path-from-shell elpy easy-hugo dash-functional counsel beacon auto-complete aggressive-indent))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
