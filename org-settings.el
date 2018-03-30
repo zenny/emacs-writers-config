ed5484379f2c6b31099db0c302a5e18d072eee1c
owl <m2eadams@gmail.com>
2018-03-30 06:43:53 +0200

    Changes org-todo-keywords to IDEA-STARTED-NEXT-WAITING-DONE

--- org-settings.el
+++ org-settings.el
@@ -52,7 +52,8 @@
 
 ;;* ===== Speed commands ======
 
-(setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)" "CANCELED(c)")))
+(setq org-todo-keywords '((sequence "IDEA(i)" "TODO(t)" "STARTED(s)" "NEXT(n)" "WAITING(w)" "|" "DONE(d)")
+			  (sequence "|" "CANCELED(c)" "DELEGATED(l)" "SOMEDAY(f)")))
 
 (setq org-enforce-todo-dependencies t)
 
