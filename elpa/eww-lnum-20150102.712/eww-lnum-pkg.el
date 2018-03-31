05d99ae43f0a860c9ab86b9d62d6bf31e33976af
owl <m2eadams@gmail.com>
2018-03-31 09:47:56 +0200

    ELPA commits

---
+++ elpa/eww-lnum-20150102.712/eww-lnum-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "eww-lnum" "20150102.712" "Conkeror-like functionality for eww" 'nil :commit "4b0ecec769919ecb05ca4fb15ec51911ba589929" :url "https://github.com/m00natic/eww-lnum" :keywords '("eww" "browse" "conkeror"))
