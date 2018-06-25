1ef3682703bc5db146f3f6989841190e5afe8058
owl <m2eadams@gmail.com>
2018-06-25 00:29:09 -0700

    Initial commit

---
+++ elpa/quelpa-use-package-20180617.2356/quelpa-use-package-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "quelpa-use-package" "20180617.2356" "quelpa handler for use-package" '((emacs "24.3") (quelpa "0") (use-package "2")) :commit "226f928c077eaff59c8bbd76398f0f6876505bc0" :keywords '("package" "management" "elpa" "use-package") :url "https://framagit.org/steckerhalter/quelpa-use-package")
