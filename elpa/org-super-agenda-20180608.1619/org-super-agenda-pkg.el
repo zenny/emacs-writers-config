88ac90ec727175ea109574c3de32b4eeede44623
owl <m2eadams@gmail.com>
2018-07-11 11:55:42 +0200

    ELPA updates and deletions

--- elpa/org-super-agenda-20180608.1619/org-super-agenda-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-super-agenda" "20180608.1619" "Supercharge your agenda" '((emacs "25.1") (s "1.10.0") (dash "2.13") (org "9.0") (ht "2.2")))
---
+++ elpa/shrink-whitespace-20150916.1215/shrink-whitespace-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "shrink-whitespace" "20150916.1215" "Whitespace removal DWIM key" 'nil)
