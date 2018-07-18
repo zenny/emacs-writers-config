708200f24ecdf76cce2588710bd3e525d51389c1
owl <m2eadams@gmail.com>
2018-07-18 13:50:21 +0200

    ELPA updates and deletions

---
+++ elpa/auto-package-update-20180712.1345/auto-package-update-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "auto-package-update" "20180712.1345" "Automatically update Emacs packages." '((emacs "24.4") (dash "2.1.0")))
---
+++ elpa/counsel-20180717.845/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20180717.845" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")))
