9a272d6c6e8c3713c1fd1353b0641192a3409321
owl <m2eadams@gmail.com>
2018-08-17 06:02:09 -0700

    ELPA updates

---
+++ elpa/git-commit-20180802.2018/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20180802.2018" "Edit Git commit messages" '((emacs "25.1") (dash "20180413") (with-editor "20180414")) :commit "fe1be8bfd54c320f2819aca8558581f13e933504" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
